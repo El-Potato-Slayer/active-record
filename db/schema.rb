1b77ff2b227fe4b464874efb3ea7c8b9172f7b27
Lidya1234 <lidya94gher@gmail.com>
2020-11-06 18:12:16 +0300

    Fix rubocop errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_094044) do
+ActiveRecord::Schema.define(version: 20_201_106_094_044) do
   create_table "comments", force: :cascade do |t|
     t.string "comment"
     t.integer "user_id", null: false
