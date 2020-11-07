8208c19d7c5c8c3ec1a99f599f19658cb886cdef
El-Potato-Slayer <leighchad.cl@gmail.com>
2020-11-08 01:21:57 +0200

    changed post attribute to body, and added validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
 
-  validates :post, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_181737) do
+ActiveRecord::Schema.define(version: 2020_11_07_231133) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comment"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_07_181737) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "post"
+    t.string "body"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
